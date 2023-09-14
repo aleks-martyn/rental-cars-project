a867e6e27fad66755d888fdbc48691db0ee4ea0c
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-15 01:02:12 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="catalog" element={CatalogPage} />
-        <Route path="favorites" element={FavoritesPage} />
+        <Route path="catalog" element={<CatalogPage />} />
+        <Route path="favorites" element={<FavoritesPage />} />
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
