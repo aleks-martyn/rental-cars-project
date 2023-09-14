a12ab42aaaa3326e6bddbb783620d501cc4807ff
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-15 00:59:54 +0300

    Create Presentation Component.

---
+++ src/components/Presentation/Presentation.jsx
@@ -0,0 +1,8 @@
+import { Wrap, Title, Description } from "./Presentation.styled";
+
+export const Presentation = () => (
+  <Wrap>
+    <Title>Rental Cars</Title>
+    <Description>Послуги компанії</Description>
+  </Wrap>
+);
---
+++ src/components/Presentation/Presentation.styled.jsx
@@ -0,0 +1,18 @@
+import styled from "@emotion/styled";
+
+export const Wrap = styled.div`
+  width: 600px;
+  height: 400px;
+  margin-left: auto;
+  margin-right: auto;
+  padding-top: 100px;
+  padding-bottom: 100px;
+  text-align: center;
+`;
+
+export const Title = styled.h1`
+  margin-bottom: 20px;
+`;
+
+export const Description = styled.p`
+`;
