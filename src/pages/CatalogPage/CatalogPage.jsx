984f54d00ceaa8294387795c57c4ad01377041a1
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-15 01:05:37 +0300

    Update CatalogPage.jsx

--- src/pages/CatalogPage/CatalogPage.jsx
+++ src/pages/CatalogPage/CatalogPage.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
+import { fetchCars } from "../../services/api";
+import { CarList } from "../../components/CarList";
+
 export default function CatalogPage() {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    const getCars = async () => {
+      try {
+        const res = await fetchCars();
+        setCars(res);
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+
+    getCars();
+  }, []);
+
   return (
-    <div>
-      <p>CatalogPage</p>
-    </div>
+    <section>
+      <CarList cars={cars} />
+    </section>
   );
 }
