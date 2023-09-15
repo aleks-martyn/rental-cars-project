a4fe4b88a9c8fff857ab5d565fa9bb22462103f2
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-15 18:34:13 +0300

    Update styles.

--- src/components/Layout/Layout.styled.jsx
+++ src/components/Layout/Layout.styled.jsx
@@ -1,12 +1,13 @@
 import styled from "@emotion/styled";
 
 export const Container = styled.div`
+  box-sizing: border-box;
   margin-left: auto;
   margin-right: auto;
   padding-left: 128px;
   padding-right: 128px;
   padding-top: 150px;
   padding-bottom: 150px;
-  max-width: 1440px;
+  width: 1440px;
   height: 100vh;
 `;
