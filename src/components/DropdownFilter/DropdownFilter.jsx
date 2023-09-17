fd1187b4b6aa12c16fc69487c8954c1e16afeac7
Aleksandr Martyniuk <aleksandr.martyniuk@gmail.com>
2023-09-17 21:20:35 +0300

    Updating DropdownMenu.

--- src/components/DropdownFilter/DropdownFilter.jsx
+++ src/components/DropdownFilter/DropdownFilter.jsx
@@ -1,62 +1,97 @@
 import {
+  StyledSection,
   StyledForm,
   Wrap,
   InputWrap,
+  InputInnerWrap,
   Subtitle,
+  StyledSelect,
+  StyledLabel,
+  StyledInput,
   SearchBtn,
 } from "./DropdownFilter.styled";
 
-export const DropdownFilter = () => {
+export const DropdownFilter = ({ onSubmit }) => {
+  const brands = [
+    { name: "Buick" },
+    { name: "Volvo" },
+    { name: "HUMMER" },
+    { name: "Subaru" },
+    { name: "Mitsubishi" },
+    { name: "Nissan" },
+    { name: "Lincoln" },
+    { name: "GMC" },
+    { name: "Hyundai" },
+    { name: "MINI" },
+    { name: "Bentley" },
+    { name: "Mercedes-Benz" },
+    { name: "Aston Martin" },
+    { name: "Pontiac" },
+    { name: "Lamborghini" },
+    { name: "Audi" },
+    { name: "BMW" },
+    { name: "Chevrolet" },
+    { name: "Chrysler" },
+    { name: "Kia" },
+    { name: "Land" },
+  ];
+
+  const prices = [
+    { quantity: "30" },
+    { quantity: "40" },
+    { quantity: "50" },
+    { quantity: "60" },
+    { quantity: "70" },
+    { quantity: "80" },
+  ];
+
   return (
-    <StyledForm name="car-search-form">
-      <Wrap>
-        <Subtitle htmlFor="car-select">Car brand</Subtitle>
+    <StyledSection>
+      <StyledForm name="car-search-form" onSubmit={onSubmit}>
+        <Wrap>
+          <Subtitle htmlFor="car-select">Car brand</Subtitle>
 
-        <select name="cars" id="car-select">
-          <option value="">Enter the text</option>
-          <option value="Buick">Buick</option>
-          <option value="Volvo">Volvo</option>
-          <option value="Hummer">Hummer</option>
-          <option value="Subaru">Subaru</option>
-          <option value="Mitsubishi">Mitsubishi</option>
-          <option value="Nissan">Nissan</option>
-          <option value="Lincoln">Lincoln</option>
-          <option value="GMC">GMC</option>
-          <option value="Hyundai">Hyundai</option>
-        </select>
-      </Wrap>
+          <StyledSelect name="cars" id="car-select">
+            <option value="">Enter the text</option>
+            {brands.map(({ name }) => (
+              <option key={name} value={name}>
+                {name}
+              </option>
+            ))}
+          </StyledSelect>
+        </Wrap>
 
-      <Wrap>
-        <Subtitle htmlFor="price-select">Price / 1 hour</Subtitle>
+        <Wrap>
+          <Subtitle htmlFor="price-select">Price / 1 hour</Subtitle>
 
-        <select name="prices" id="price-select">
-          <option value="">To $</option>
-          <option value="30">30</option>
-          <option value="40">40</option>
-          <option value="50">50</option>
-          <option value="60">60</option>
-          <option value="70">70</option>
-          <option value="80">80</option>
-        </select>
-      </Wrap>
+          <StyledSelect name="prices" id="price-select">
+            <option value="">To $</option>
+            {prices.map(({ quantity }) => (
+              <option key={quantity} value={quantity}>
+                {quantity}
+              </option>
+            ))}
+          </StyledSelect>
+        </Wrap>
 
-      <Wrap>
-        <Subtitle>Car mileage / km</Subtitle>
+        <Wrap>
+          <Subtitle>Car mileage / km</Subtitle>
 
-        <InputWrap>
-          <label>
-            From
-            <input type="number" name="from" />
-          </label>
+          <InputWrap>
+            <InputInnerWrap>
+              <StyledLabel htmlFor="from">From</StyledLabel>
+              <StyledInput type="number" name="from" id="from" />
+            </InputInnerWrap>
 
-          <label>
-            To
-            <input type="number" name="to" />
-          </label>
-        </InputWrap>
-      </Wrap>
+            <InputInnerWrap>
+              <StyledLabel htmlFor="to">To</StyledLabel>
+              <StyledInput type="number" name="to" id="to" />
+            </InputInnerWrap>
+          </InputWrap>
+        </Wrap>
 
-      <SearchBtn type="submit">Search</SearchBtn>
-    </StyledForm>
+        <SearchBtn type="submit">Search</SearchBtn>
+      </StyledForm>
+    </StyledSection>
   );
 };
--- src/components/DropdownFilter/DropdownFilter.styled.jsx
+++ src/components/DropdownFilter/DropdownFilter.styled.jsx
@@ -1,8 +1,13 @@
 import styled from "@emotion/styled";
 
+export const StyledSection = styled.section`
+  padding-top: 100px;
+`;
+
 export const StyledForm = styled.form`
   display: flex;
   gap: 18px;
+  justify-content: space-around;
   width: 859px;
   height: 74px;
   margin-left: auto;
@@ -20,11 +25,40 @@ export const InputWrap = styled.div`
   display: flex;
 `;
 
+export const InputInnerWrap = styled.div`
+  position: relative;
+`;
+
 export const Subtitle = styled.label`
   font-weight: 500;
   font-size: 14px;
   line-height: 1.29;
   color: var(--secondary-text-color);
+`;
+
+export const StyledSelect = styled.select`
+  height: 48px;
+  padding: 14px 18px;
+  border: none;
+  border-radius: 14px;
+  background-color: var(--select-color);
+`;
+
+export const StyledLabel = styled.label`
+  position: absolute;
+  top: 14px;
+  left: 14px;
+`;
+
+export const StyledInput = styled.input`
+  box-sizing: border-box;
+  width: 160px;
+  height: 48px;
+  padding: 14px 18px;
+  padding-left: 60px;
+  border: none;
+  border-radius: 14px;
+  background-color: var(--select-color);
 `;
 
 export const SearchBtn = styled.button`
